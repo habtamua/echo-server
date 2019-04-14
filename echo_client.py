a8460f31a3a68e2344f7911c2bcf8f44ee42d158
HABTAMU ASFAW <asfawh@uw.edu>
2019-04-14 01:18:05 -0700

    refactor to meet unittest

--- echo_client.py
+++ echo_client.py
@@ -16,24 +16,27 @@ def client(msg, log_buffer=sys.stderr):
         sock.connect(server_address)
         print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
         # Send data
-        message = b'this is the message. It will be repeated'
+        # message = b'this is the message. It will be repeated'
+        recvdata = b''
         try:
             print('sending "{0}"'.format(msg), file=log_buffer)
-            sock.sendall(message)
+            sock.sendall(msg.encode('utf8'))
             # Look for the response
             amount_received = 0
-            amount_expected = len(message)
+            amount_expected = len(msg)
             while amount_received < amount_expected:
                 data = sock.recv(16)
-                amount_received += len(data)
-                print('received "{0}"'.format(data.decode('utf8')), file=log_buffer)
+                recvdata += data
+                amount_received += len(recvdata)
+                print('received "{0}"'.format(recvdata.decode('utf8')), file=log_buffer)
         except BrokenPipeError as err:
             traceback.print_exc(err)
             sys.exit(1)
         finally:
             print('closing socket', file=log_buffer)
             # sock.close()
-        return data
+        # return msg
+        return recvdata.decode('utf8')
 
 
 if __name__ == '__main__':
